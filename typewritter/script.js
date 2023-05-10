2f8fb68d4c5f9bbf52e0059117553f0ae772a4d6
vicky42119 <beyrv416@newschool.edu>
2023-05-10 00:07:02 -0400

    update

--- typewritter/script.js
+++ typewritter/script.js
@@ -1,11 +1,33 @@
 
   const keyImageMap = {
-    'A': '../a.png',
+    'A': 'a.png',
     'B': 'b.png',
-    'C': '../letters/c.png',
-    'D': '../letters/d.png',
-    'E': '../letters/e.png',
-    'F': '../letters/f.png',
+    'C': 'c.png',
+    'D': 'd.png',
+    'E': 'e.png',
+    'F': 'f.png',
+    'G': 'g.png',
+    'H': 'h.png',
+    'I': 'i.png',
+    'J': 'j.png',
+    'K': 'k.png',
+    'L': 'l.png',
+    'M': 'm.png',
+    'N': 'n.png',
+    'O': 'o.png',
+    'P': 'p.png',
+    'Q': 'q.png',
+    'R': 'r.png',
+    'S': 's.png',
+    'T': 't.png',
+    'U': 'u.png',
+    'V': 'v.png',
+    'W': 'w.png',
+    'X': 'x.png',
+    'Y': 'y.png',
+    'Z': 'z.png',
+    ' ': 'nothing.png', // add space key mapping here
+
     // add more key-image mappings here
   };
   
--- web_3/modular-letters5.js
+++ web_3/modular-letters5.js
@@ -1,4 +1,6 @@
 // Get all of the .canvas-group elements from the page, and put it in an array
+
+
 var canvases = [] 
 
 var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
@@ -7,11 +9,11 @@ var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F
   img.src = deleteIcon;
   
   fabric.Object.prototype.transparentCorners = true;
-  fabric.Object.prototype.cornerColor = 'white';
+  fabric.Object.prototype.cornerColor = 'black';
   fabric.Object.prototype.cornerStyle = 'circle';
 
 var drawGrid = function(canvasToWorkOn) {
-    var gridSize = 20;
+    var gridSize = 22.5;
     for (var j = 0; j < canvasToWorkOn.width / gridSize; j++) {
         canvasToWorkOn.add(new fabric.Line([ j * gridSize, 0, j * gridSize, canvasToWorkOn.height], { stroke: 'black', selectable: false, opacity: 0.5 }));
     }
@@ -19,6 +21,7 @@ var drawGrid = function(canvasToWorkOn) {
         canvasToWorkOn.add(new fabric.Line([ 0, k * gridSize, canvasToWorkOn.width, k * gridSize], { stroke: 'black', selectable: false, opacity: 0.5 }));
     }
 };
+
 
 var saveLetter = function(canvas) {
   // Create a new canvas with a transparent background
@@ -172,8 +175,8 @@ var addSemiCircle = function(canvas) {
 
 var fabricOptions = {
   backgroundColor: 'white', // Set the canvas background color
-  width: 400, // Set the canvas width
-  height: 400, // Set the canvas height
+  width: 450, // Set the canvas width
+  height: 450, // Set the canvas height
   selection: false // Disable object selection
 };
 
@@ -292,6 +295,8 @@ fabric.Object.prototype.controls.deleteControl = new fabric.Control({
     ctx.drawImage(img, -size/2, -size/2, size, size);
     ctx.restore();
   }
+
+  
 
 
 
